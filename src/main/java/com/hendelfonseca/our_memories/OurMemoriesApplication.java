49d4b682909de3d700c0263488f1fb92d1dc776b
Hendel Fonseca <hendeletfonseca@gmail.com>
2024-10-02 15:52:15 -0300

    Spring Initializr

---
+++ src/main/java/com/hendelfonseca/our_memories/OurMemoriesApplication.java
@@ -0,0 +1,13 @@
+package com.hendelfonseca.our_memories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurMemoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurMemoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hendelfonseca/our_memories/OurMemoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendelfonseca.our_memories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurMemoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
